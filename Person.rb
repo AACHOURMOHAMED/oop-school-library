fe3afadee6c0e43fd7509afaa7fe7d549f0f41d7
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2022-09-29 11:37:42 +0100

    fix linter error

--- Person.rb
+++ Person.rb
@@ -1,50 +1,51 @@
 class Person
-    attr_accessor :id, :age, :name
+  attr_accessor :id, :age, :name
 
-    def initialize(id, age, name: 'Unkown', parent_permission: true)
-        @id = id
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
-    # getters for @id @name @age
+  def initialize(id, age, name: 'Unkown', parent_permission: true)
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+  # getters for @id @name @age
 
-    def id 
-        @id
-    end
+  def id
+    @id
+  end
 
-    def name
-        @name
-    end
-    
-    def age 
-        @age
-    end
+  def age
+    @age
+  end
 
-    #setters for @id @name @age
+  def name
+    @name
+  end
 
-    def id=(id)
-        @id = id
-    end
+  # setters for @id @name @age
 
-    def name=(name)
-        @name = name
-    end
+  def id=(id)
+    @id = id
+  end
 
-    def age=(age)
-        @age = age
-    end
-    #private method
-    private
-     
-    def is_of_age
-        @age >= 18
-    end
+  def age=(age)
+    @age = age
+  end
 
+  def name=(name)
+    @name = name
+  end
 
-    # public methods
+  # private method
 
-    def can_use_services
-        is_of_age || @parent_permission
-    end
+  private
+
+  def is_of_age
+    @age >= 18
+  end
+
+  # public methods
+
+  def can_use_services
+    is_of_age || @parent_permission
+  end
 end
