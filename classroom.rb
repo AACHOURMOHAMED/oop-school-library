b801b5f242d173019760741d18fb19069f48d401
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2022-09-30 07:20:09 +0100

    Add (book clasroom rental) classes

---
+++ classroom.rb
@@ -0,0 +1,18 @@
+require_relative 'students'
+
+class ClassRoom < Student
+    attr_accessor :label
+    attr_reader :students
+    has_many :students
+    
+    def initialize(label)
+        @label = label
+    end
+
+
+    def add_student(student)
+        @students = []
+        @students.push(student)
+        student.classroom = self
+    end
+end
--- students.rb
+++ students.rb
@@ -1,12 +1,21 @@
 require_relative 'persons'
+require_relative 'classroom'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
 
+
   def play_hook
     "¯\(ツ)/¯"
+  end
+
+  def add_classroom = (classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
