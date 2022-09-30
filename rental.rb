4f34ccd42494752137d50f7b83982c04bb8ff85d
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2022-09-30 19:08:13 +0100

    Add ui to oop School libarary

--- rental.rb
+++ rental.rb
@@ -1,11 +1,11 @@
 class Rental
-  attr_accessor :date
+  attr_accessor :date, :book, :person
 
   def initialize(date, book, person)
     @date = date
     @book = book
-    book.rentals << self
     @person = person
-    person.rentals << self
+    @book.add_rental(self)
+    @person.add_rental(self)
   end
 end
