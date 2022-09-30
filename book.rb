8a93f62a7a95eaec589c45c075e5fa8e862f0c3f
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2022-09-30 07:21:05 +0100

    Add add_rental and modify student

--- book.rb
+++ book.rb
@@ -1,16 +1,17 @@
 require_relative 'rental'
 
 class Book
-    attr_accessor :title, :author
-    attr_reader :rentals
-    def initialize(title, author)
-        @title = title
-        @author = author
-        @rentals = []
-    end
+  attr_accessor :title, :author
+  attr_reader :rentals
 
-    def add_rental(person , date)
-        Rental.new(date, self, person)
-        @rentals.push(rental)
-    end
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(person, date)
+    Rental.new(date, self, person)
+    @rentals.push(rental)
+  end
 end
