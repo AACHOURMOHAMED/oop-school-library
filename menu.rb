1b7e9c8e91cd0a0b5a686438129fa1201517151f
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2022-10-04 15:04:21 +0100

    Add store data to json file

--- menu.rb
+++ menu.rb
@@ -5,6 +5,8 @@ require_relative 'app'
 APP = App.new
 
 class Menu
+    # call load_data method to load data from json file
+    APP.load_data
   def menu_message
     puts '____________________________________________________________'
     puts 'Please choose an option by entering a number:'
@@ -16,29 +18,31 @@ class Menu
     puts '6 - List all rentals for a given person id'
     puts '7 - Exit'
   end
-  
+
   def display_menu # rubocop:disable Metrics/CyclomaticComplexity
-        menu_message
-        option = gets.chomp.to_i
-        case option
-        when 1
-        APP.list_books
-        when 2
-        APP.list_people
-        when 3
-        APP.create_person
-        when 4
-        APP.create_book
-        when 5
-        APP.create_rental
-        when 6
-        APP.list_rentals_by_person_id
-        when 7
-        puts 'Thank you for using this app!'
-        exit
-        else puts 'Invalid option' end
-        display_menu
-    end
+    menu_message
+    option = gets.chomp.to_i
+    case option
+    when 1
+      APP.list_books
+    when 2
+      APP.list_people
+    when 3
+      APP.create_person
+    when 4
+      APP.create_book
+    when 5
+      APP.create_rental
+    when 6
+      APP.list_rentals_by_person_id
+    when 7
+      puts 'Thank you for using this app!'
+      # save all data to json file
+        APP.save_data
+      exit
+    else puts 'Invalid option' end
+    display_menu
+  end
 end
 
 main = Menu.new
