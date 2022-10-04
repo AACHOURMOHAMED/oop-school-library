d89b824cb095a2bf20e0f4dbf2de38b9950dc9fe
Hamma Azarok <hammaagazarok@gmail.com>
2022-10-04 10:22:22 +0200

    implement SRP

--- menu.rb
+++ menu.rb
@@ -16,6 +16,29 @@ class Menu
     puts '6 - List all rentals for a given person id'
     puts '7 - Exit'
   end
+  
+  def display_menu # rubocop:disable Metrics/CyclomaticComplexity
+        menu_message
+        option = gets.chomp.to_i
+        case option
+        when 1
+        APP.list_books
+        when 2
+        APP.list_people
+        when 3
+        APP.create_person
+        when 4
+        APP.create_book
+        when 5
+        APP.create_rental
+        when 6
+        APP.list_rentals_by_person_id
+        when 7
+        puts 'Thank you for using this app!'
+        exit
+        else puts 'Invalid option' end
+        display_menu
+    end
 end
 
 main = Menu.new
