586c7e0b3bb95248e3d63d78c0cc689cf0d5d9f7
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2022-10-05 15:27:00 +0100

    Add test for book and person

---
+++ test_spec.rb
@@ -0,0 +1,100 @@
+require_relative 'book'
+require_relative 'persons'
+require_relative 'students'
+require_relative 'teachers'
+require_relative 'classroom'
+require_relative 'rental'
+
+describe Book do
+  let(:book) { Book.new('The Great Gatsby', 'F. Scott Fitzgerald') }
+
+  describe '#new' do
+    it 'creates a new book' do
+      expect(book).to be_an_instance_of(Book)
+    end
+
+    it 'has a title' do
+      expect(book.title).to eq('The Great Gatsby')
+    end
+
+    it 'has an author' do
+      expect(book.author).to eq('F. Scott Fitzgerald')
+    end
+  end
+end
+
+describe Person do
+  let(:person) { Person.new(20, 'John Doe') }
+
+  describe '#new' do
+    it 'creates a new person' do
+      expect(person).to be_an_instance_of(Person)
+    end
+
+    it 'has an age' do
+      expect(person.age).to eq(20)
+    end
+
+    it 'has a name' do
+      expect(person.name).to eq('John Doe')
+    end
+  end
+end
+
+describe Student do
+  let(:student) { Student.new(20, 'John Doe') }
+
+  describe '#new' do
+    it 'creates a new student' do
+      expect(student).to be_an_instance_of(Student)
+    end
+
+    it 'has an age' do
+      expect(student.age).to eq(20)
+    end
+
+    it 'has a name' do
+      expect(student.name).to eq('Unknown')
+    end
+  end
+end
+
+describe Teacher do
+  let(:teacher) { Teacher.new(20, 'John Doe') }
+
+  describe '#new' do
+    it 'creates a new teacher' do
+      expect(teacher).to be_an_instance_of(Teacher)
+    end
+
+    it 'has an age' do
+      expect(teacher.age).to eq(20)
+    end
+
+    it 'has a name' do
+      expect(teacher.name).to eq('Unknown')
+    end
+  end
+end
+
+describe ClassRoom do
+  let(:classroom) { ClassRoom.new('Math') }
+
+  describe '#new' do
+    it 'creates a new classroom' do
+      expect(classroom).to be_an_instance_of(ClassRoom)
+    end
+  end
+end
+
+describe Rental do
+  let(:rental) {
+    Rental.new('2021-21-12', Book.new('The Great Gatsby', 'F. Scott Fitzgerald'), Person.new(20, 'John Doe'))
+  }
+
+  describe '#new' do
+    it 'creates a new rental' do
+      expect(rental).to be_an_instance_of(Rental)
+    end
+  end
+end
